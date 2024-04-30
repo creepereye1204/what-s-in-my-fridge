2858e0442439c0196de11b1f3fab0150fb60bc3f
Karmesin924 <jinhong0924@naver.com>
2024-04-30 13:20:16 +0900

    init: Android 프로젝트 생성

---
+++ app/src/test/java/com/web/whats_in_my_fridge/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.web.whats_in_my_fridge
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
